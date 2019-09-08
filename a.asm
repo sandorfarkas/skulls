80d1e6fe626cdd90dd6d16f5a5b46c7399f96a9a
sandorfarkas <sandor.farkas@gmail.com>
2019-09-08 21:12:56 +0200

    pure asm loop

--- a.asm
+++ a.asm
@@ -4,149 +4,33 @@ start:  lda #$07
 		sta $d020	// set background to 7
 		lda #$ff
 		sta $d015  	// turn on all sprites
-		lda #$d     // 13 * 64 -> 832  (D h -> 13 d)
+		lda #$0d    // 13 * 64 -> 832  (D h -> 13 d)
 		sta $07f8	// set sprite data location ($07f8 h -> 2040 d)
 
-		// lda #$00
-		// sta $0340
-		// lda #$00
-		// sta $0341
-		// lda #$00
-		// sta $0342
-		// lda #$20
-		// sta $0343
-		// lda #$00
-		// sta $0344
-		// lda #$04
-		// sta $0345
-		// lda #$60
-		// sta $0346
-		// lda #$00
-		// sta $0347
-		// lda #$06
-		// sta $0348
-		// lda #$11
-		// sta $0349
-		// lda #$ff
-		// sta $034a
-		// lda #$88
-		// sta $034b
-		// lda #$09
-		// sta $034c
-		// lda #$00
-		// sta $034d
-		// lda #$90
-		// sta $034e
-		// lda #$01
-		// sta $034f
-		// lda #$00
-		// sta $0350
-		// lda #$80
-		// sta $0351
-		// lda #$01
-		// sta $0352
-		// lda #$00
-		// sta $0353
-		// lda #$80
-		// sta $0354
-		// lda #$0f
-		// sta $0355
-		// lda #$7e
-		// sta $0356
-		// lda #$f0
-		// sta $0357
-		// lda #$08
-		// sta $0358
-		// lda #$00
-		// sta $0359
-		// lda #$10
-		// sta $035a
-		// lda #$08
-		// sta $035b
-		// lda #$24
-		// sta $035c
-		// lda #$10
-		// sta $035d
-		// lda #$08
-		// sta $035e
-		// lda #$00
-		// sta $035f
-		// lda #$10
-		// sta $0360
-		// lda #$08
-		// sta $0361
-		// lda #$00
-		// sta $0362
-		// lda #$10
-		// sta $0363
-		// lda #$08
-		// sta $0364
-		// lda #$00
-		// sta $0365
-		// lda #$10
-		// sta $0366
-		// lda #$0a
-		// sta $0367
-		// lda #$a5
-		// sta $0368
-		// lda #$50
-		// sta $0369
-		// lda #$00
-		// sta $036a
-		// lda #$00
-		// sta $036b
-		// lda #$00
-		// sta $036c
-		// lda #$00
-		// sta $036d
-		// lda #$00
-		// sta $036e
-		// lda #$00
-		// sta $036f
-		// lda #$02
-		// sta $0370
-		// lda #$00
-		// sta $0371
-		// lda #$40
-		// sta $0372
-		// lda #$04
-		// sta $0373
-		// lda #$00
-		// sta $0374
-		// lda #$20
-		// sta $0375
-		// lda #$18
-		// sta $0376
-		// lda #$00
-		// sta $0377
-		// lda #$18
-		// sta $0378
-		// lda #$08
-		// sta $0379
-		// lda #$00
-		// sta $037a
-		// lda #$10
-		// sta $037b
-		// lda #$00
-		// sta $037c
-		// lda #$00
-		// sta $037d
-		// lda #$00
-		// sta $037e
-		// lda #$03
-		// sta $037f
-
+		// set sprite 0 pos x,y
 		lda #$64
 		sta $d000	// sprite 0 x
 		lda #$64
 		sta $d001 	// sprite 0 y
-loop:	inc $d000
-		.for(var j=0; j<10000; j++) {
-			nop
-		}		
-		jmp loop		
 
-		*=$0340 "Sprite data"
+		lda #$ff
+		sta $0800
+loop:	inc $d000
+		lda #$ff
+		sta $0801		
+inner_loop:
+        nop
+		dec $0801
+		lda $0801
+		cmp #$00
+		bne inner_loop
+		dec $0800
+		lda $0800
+		cmp #$00
+		bne loop
+		jmp start
+//----------------------------------------------------------
+		* = $0340 "Sprite data"
 sprite_0:
 		.byte $00,$00,$00,$20,$00,$04,$60,$00
 		.byte $06,$11,$ff,$88,$09,$00,$90,$01
@@ -164,4 +48,3 @@ sinus:
  		.byte  -13,-14,-15,-15,-16,-17,-18,-18,-19,-19,-19,-20
  		.byte  -20,-20,-20,-20,-20,-20,-19,-19,-19,-18,-18,-17
  		.byte  -16,-15,-15,-14,-13,-12,-11,-10,-9,-7,-6,-5,-4,-3,-1
-		jmp *
