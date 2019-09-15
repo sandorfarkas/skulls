51d199528bc923d7692e60e905fa0b0c6c4f4bf9
sandorfarkas <sandor.farkas@gmail.com>
2019-09-15 22:25:42 +0200

    Skull moves across whole screen

--- skulls.asm
+++ skulls.asm
@@ -1,46 +1,81 @@
-BasicUpstart2(start)
+BasicUpstart2(init)
+
         * = $4000 "Main Program"
-start:  lda #$07
+init: 	lda #$07
 		sta $d020	// set background to 7
-		lda #$ff
-		sta $d015  	// turn on all sprites
-		lda #$0d    // 13 * 64 -> 832  (D h -> 13 d)
-		sta $07f8	// set sprite data location ($07f8 h -> 2040 d)
+		lda #%00000001
+		sta $d015  	// turn on sprite 0
+		lda #$0d    // 13 * 64 -> 832  ($d -> 13)
+		sta $07f8	// set sprite data location ($07f8 -> 2040)
 
 		// set sprite 0 pos x,y
-		lda #$64
+		lda #$00
 		sta $d000	// sprite 0 x
-		lda #$64
+		lda $5002
 		sta $d001 	// sprite 0 y
 
-		lda #$ff
-		sta $0800   // loop counter starting from $ff
+start:	lda #$ff
+		sta $5000   // loop counter starting from $ff
 loop:	inc $d000	
-		ldx $d000   // copy x pos to X
-		lda $5000,X // copy sin(X) to A
-		adc #$64    // add $64 to A
-		sta $d001
+		ldx $d000   // copy x pos to x
+		ldy $d010
+		cpy #$ff
+		beq adjust_x_to_read_sine
+back_from_adjust_x:
+		lda $5002,x // copy sin(x) to a
+		adc #$64    // add $64 to a
+		sta $d001   // set pos y
 
+		jmp set_msb_on
+back_from_msb_on:		
+		jmp set_msb_off
+back_from_msb_off:
 		lda #$ff
-		sta $0801	// inner loop counter starting from $ff
+		sta $5001	// inner loop counter starting from $ff
 inner_loop:
-        nop
-        nop
-        nop
-        nop
-        nop
-		dec $0801
-		lda $0801
-		cmp #$00
+		nop
+		.for(var i=0;i<20;i++) nop 
+		dec $5001
 		bne inner_loop
 		
-		dec $0800
-		lda $0800
-		cmp #$00
+		dec $5000
 		bne loop
 		
 		jmp start
-//----------------------------------------------------------
+
+adjust_x_to_read_sine:
+		pha
+		txa
+		adc #$37 // dec 55
+		tax
+		pla
+		jmp back_from_adjust_x
+
+set_msb_on:
+		lda $d000
+		cmp #$ff
+		bne back_from_msb_on 
+		// there is $ff in a
+		sta $d010
+		lda #$00
+		sta $d000
+		jmp back_from_msb_off
+
+set_msb_off:
+		lda $d010
+		cmp #$ff
+		bne back_from_msb_off
+		lda $d000
+		cmp #$54
+		beq off
+		jmp back_from_msb_off
+
+off: 	lda #$00
+		sta $d010
+		lda #$00
+		sta $d000
+		jmp back_from_msb_off
+
 		* = $0340 "Sprite data"
 		.byte $00,$00,$00,$20,$00,$04,$60,$00
 		.byte $06,$11,$ff,$88,$09,$00,$90,$01
@@ -50,7 +85,8 @@ inner_loop:
 		.byte $a5,$50,$00,$00,$00,$00,$00,$00
 		.byte $02,$00,$40,$04,$00,$20,$18,$00
 		.byte $18,$08,$00,$10,$00,$00,$00,$03
-		* = $5000 "Sin data"
+
+		* = $5002 "Sine data"
 		.byte $14,$15,$17,$18,$19,$1A,$1B,$1D
 		.byte $1E,$1F,$20,$21,$22,$23,$23,$24
 		.byte $25,$26,$26,$27,$27,$27,$28,$28
